17fbe8372b213c5755c97226c9b40b44c90a6af0
DoubleZero <TitovM157@gmail.com>
2025-10-06 22:31:11 +0300

    Change main.py

--- src/DS_ITMO_Course/main.py
+++ src/DS_ITMO_Course/main.py
@@ -5,77 +5,45 @@
 Добавлен для проверки вывод info()
 """
 
-import sys
-import os
 from data_loader import load_data, check_xlsx_file
 from data_conversion import conversion_of_data
 from data_saver import save_parquet, check_parquet_file, load_parquet
 
-docs_dir = os.path.join(os.path.dirname(__file__), "docs")
-os.makedirs(docs_dir, exist_ok=True)
+print(f"\n{'-'*60}")
+print("Этап 0: Проверка наличия dataset в формате parquet")
+print(f"{'-'*60}")
+parquet_dataset = check_parquet_file()
 
-with open(os.path.join(docs_dir, "results.txt"), "w", encoding="utf-8") as f:
+if parquet_dataset:
+    print("Найден файл .parquet")
+    data = load_parquet()
+    print(f"\n{'-' * 60}")
+    print("Этап 3: Вывод информации dataset после приведения типов")
+    print(f"{'-' * 60}")
+    print("\nИнформация о типах данных:")
+    print(data.info())
 
-    class TXT_and_terminal:
-        def __init__(self, *files):
-            self.files = files
+else:
+    print(f"\n{'-' * 60}")
+    print("Этап 1: Проверка наличия dataset в формате xlsx и его загрузка")
+    print(f"{'-' * 60}")
+    print("Parquet файл не найден")
 
-        def write(self, obj):
-            for f in self.files:
-                f.write(obj)
-                f.flush()
+    if not check_xlsx_file():
+        print("xlsx файл не найден")
 
-        def flush(self):
-            for f in self.files:
-                f.flush()
+    raw_data = load_data()
+    print(f"\n{'-' * 60}")
+    print("Этап 2: Вывод информации dataset до приведения типов")
+    print(f"{'-' * 60}")
 
-    stdout = sys.stdout  # stdout для вывода в файл
+    print(raw_data.info())
 
-    try:
-        sys.stdout = TXT_and_terminal(stdout, f)
+    data = conversion_of_data(raw_data)
 
-        print(f"\n{'-'*60}")
-        print("Этап 0: Проверка наличия dataset в формате parquet")
-        print(f"{'-'*60}")
-        parquet_dataset = check_parquet_file()
+    f_path = save_parquet(data)
+    print(f"\n{'-' * 60}")
+    print("Этап 3: Вывод информации dataset после приведения типов")
+    print(f"{'-' * 60}")
 
-        if parquet_dataset:
-            print(f"Найден файл .parquet")
-            data = load_parquet()
-            print(f"\n{'-' * 60}")
-            print("Этап 3: Вывод информации dataset после привидения типов")
-            print(f"{'-' * 60}")
-            print("\nИнформация о типах данных:")
-            print(data.info())
-
-        else:
-            print(f"\n{'-' * 60}")
-            print("Этап 1: Проверка наличия dataset в формате xlsx и его загрузка")
-            print(f"{'-' * 60}")
-            print("Parquet файл не найден")
-
-            if not check_xlsx_file():
-                print("xlsx файл не найден")
-
-            raw_data = load_data()
-            print(f"\n{'-' * 60}")
-            print("Этап 2: Вывод информации dataset до привидения типов")
-            print(f"{'-' * 60}")
-
-            print(raw_data.info())
-
-            data = conversion_of_data(raw_data)
-
-            f_path = save_parquet(data)
-            print(f"\n{'-' * 60}")
-            print("Этап 3: Вывод информации dataset после привидения типов")
-            print(f"{'-' * 60}")
-
-            print(data.info())
-
-    finally:
-        sys.stdout = stdout
-
-print(f"\n{'-' * 60}")
-print(f"Результаты сохранены в: {os.path.join(docs_dir, 'results.txt')}")
-print(f"{'-' * 60}")
+    print(data.info())
